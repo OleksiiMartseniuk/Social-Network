37f22863b802f8b6132dc5f885d5d49ee0717733
alex <martsenyk.l@gmail.com>
2021-10-22 12:33:54 +0300

    create password_change

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,14 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    # Шаблоны Login
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
     path('', views.dashboard, name='dashboard'),
+
+    # Шаблоны для доступа к обработчикам смены пароля
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
 ]
