9895034602351e2817a804f358f16c5c1fa67bc6
alex <martsenyk.l@gmail.com>
2021-10-23 16:29:07 +0300

    Connecting the notification system

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from . import forms
 from .models import Profile
+from django.contrib import messages
 
 
 @login_required
@@ -39,6 +40,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = forms.UserEditForm(instance=request.user)
         profile_form = forms.ProfileEditForm(instance=request.user.profile)
