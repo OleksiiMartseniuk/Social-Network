110f00e098fc99eeb31f4d4dcb53b9666ed78874
alex <martsenyk.l@gmail.com>
2021-10-31 13:14:06 +0200

    Displaying news feed

--- account/views.py
+++ account/views.py
@@ -9,11 +9,24 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from actions.utils import create_action
+from actions.models import Action
 
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    # По умолчанию отображаем все действия.
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        # Если текущий пользователь подписался на кого-то,
+        # отображаем только действия этих пользователей.
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions[:10]
+    context = {
+        'section': 'dashboard',
+        'actions': actions
+    }
+    return render(request, 'account/dashboard.html', context)
 
 
 def register(request):
